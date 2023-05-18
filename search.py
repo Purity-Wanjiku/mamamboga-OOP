2908ab31ec557c63d15adfb37732db9d562287c6
Masian-Parkire <124132282+Masian-Parkire@users.noreply.github.com>
2023-05-18 22:27:35 +0300

    wrote methods to enable the user to search for the availability of an item and if it is there it confirms its availability if not it says it is not there. It also allows user to update unavailable  items to the list

--- search.py
+++ search.py
@@ -1,16 +1,25 @@
+class Product:
+    def __init__(self, name, available):
+        self.name = name
+        self.available = available
+    
+    def check_availability(self):
+        if self.available:
+            return f"The {self.name} is available."
+        else:
+            return f"Sorry, the {self.name} is not available."
 
-class DictionarySearch:
-    def __init__(self, products):
-        self.products = products
-    def search_product(self, key, value):
-        for product, amount in self.products.items():
-            if product == key and amount == value:
-                return (product, amount)
-        return None
-my_products = {'apple': 5, 'mango': 6, 'banana': 9}
-search = DictionarySearch(my_products)
-result = search.search_product('banana', 9)
-if result:
-    print(f"({result[0]}, {result[1]}) is available")
-else:
-    print("product not found.")
+class ProductManager:
+    def __init__(self):
+        self.products = []
+    
+    def add_product(self, name, available):
+        product = Product(name, available)
+        self.products.append(product)
+    
+    def search_product(self, name):
+        for product in self.products:
+            if product.name == name:
+                return product.check_availability()
+        return f"Sorry, the {name} is not found ."
+
