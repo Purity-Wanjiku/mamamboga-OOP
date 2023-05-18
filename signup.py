a3ca5de2ab9bcfd178666e36f7a9d0bb8b23b40e
Masian-Parkire <124132282+Masian-Parkire@users.noreply.github.com>
2023-05-18 20:25:48 +0300

    Update signup.py

--- signup.py
+++ signup.py
@@ -1,26 +1,26 @@
 
    
 class Signup:
-    def __init__(self,First_Name,Last_Name,Email_Address,Phone_No,Account_password,Password_Confirm) :
-        self.First_Name=First_Name
-        self.Last_Name=Last_Name
-        self.Email_Address=Email_Address
-        self.Phone_No=Phone_No
-        self.Account_password=Account_password   
-        self.Password_Confirm=Password_Confirm
+    def __init__(self,first_Name,last_name,email_address,phone_no,account_password,password_confirm) :
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email_address=email_address
+        self.phone_no=phone_no
+        self.account_password=account_password   
+        self.password_confirm=password_confirm
        
        #logging in after creating an account
 
     def validate_password(self):
-        if(self.Account_password==self.Password_Confirm):
+        if(self.account_password==self.password_confirm):
             print( f"Valid password")
         else:
             print(f"Please enter the correct password")
 
     def log_in (self,email,password):
-        if email== self.Email_Address:
+        if email== self.email_address:
             return (f"Email successful")   
-        elif password== self.Account_password:
+        elif password== self.account_password:
             return(f"Password successful")
         else :
             return(f"Your email does not match your password")
