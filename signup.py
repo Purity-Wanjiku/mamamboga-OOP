be66898478b2e35e2a4e6a70bcfadc99236ac4cf
Masian-Parkire <124132282+Masian-Parkire@users.noreply.github.com>
2023-05-18 19:10:24 +0300

    Update signup.py
    the sign up class which has firstname ,lastname,phone.no,password and password
    confirmation as well as delivery location enables first first users to set up their 
     It also confirms  whether the password input is correct through the password 
     confirmation method.
     upon successfully setting up an account the user proceeds to log in into the groceries

--- signup.py
+++ signup.py
@@ -1,32 +1,27 @@
-class User:
-    def __init__(self, username, email, password):
-        self.username = username
-         self.email = email
-        self.password = password
 
-class UserManager:
-    def __init__(self):
-        self.users = []
-    
-    def add_user(self, username, email, password):
-        user = User( username, email, password)
-        self.users.append(user)
-    
-    def get_user_by_username(self, username):
-        for user in self.users:
-            if user.username == username:
-                return user
-        return None
+   
+class signup:
+    def __init__(self,First_Name,Last_Name,Email_Address,Phone_No,Account_password,Password_Confirm) :
+        self.First_Name=First_Name
+        self.Last_Name=Last_Name
+        self.Email_Address=Email_Address
+        self.Phone_No=Phone_No
+        self.Account_password=Account_password   
+        self.Password_Confirm=Password_Confirm
+       
+        
+# password confirmation checks if the account password is correct
 
-# Example usage:
-user_manager = UserManager()
+    def validate_password(self):
+        if(self.Account_password==self.Password_Confirm):
+            print( f"Valid password")
+        else:
+            print(f"Please enter the correct password")
 
-# Sign up a new user
-user_manager.add_user('val_buraje', 'val@gmail.com', 'password123')
-
-# Verify the user's login credentials
-user = user_manager.get_user_by_username('val_buraje')
-if user and user.password == 'password123':
-    print('Login successful!')
-else:
-    print('Incorrect username or password.')
+    def log_in (self,email,password):
+        if email== self.Email_Address:
+            return (f"Email successful")   
+        elif password== self.Account_password:
+            return(f"Password successful")
+        else :
+            return(f"Your email does not match your password")
