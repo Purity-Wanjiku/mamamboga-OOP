1947c0ee03138db8ff1f106da07260862dd4e9f3
Masian-Parkire <124132282+Masian-Parkire@users.noreply.github.com>
2023-05-19 15:24:18 +0300

    wrote methods and functions to enable my user update availability of products and also search if an object is there before updating it

--- product.py
+++ product.py
@@ -28,3 +28,31 @@ class ItemsAvailable:
 item = ItemsAvailable("pineapple", 200, False, {"pineapple": 180, "mango": 100})
 
 print(item.add_item(False))
+
+
+class Product:
+    def __init__(self, name, available):
+        self.name = name
+        self.available = available
+    
+    def check_availability(self):
+        if self.available:
+            return f"The {self.name} is available."
+        else:
+            return f"Sorry, the {self.name} is not available."
+
+class ProductManager:
+    def __init__(self):
+        self.products = []
+    
+    def add_product(self, name, available):
+        product = Product(name, available)
+        self.products.append(product)
+        return "item added successfully"
+    
+    def search_product(self, name):
+        for product in self.products:
+            if product.name == name:
+                return product.check_availability()
+        return f"Sorry, the {name} is not found ."
+
