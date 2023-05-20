f3aa83e313a37ae34ced16cbc615a321145757e6
Masian-Parkire <124132282+Masian-Parkire@users.noreply.github.com>
2023-05-20 23:17:29 +0300

    wrote methods to my user to get the number of orders made and the name of the person who has ordered .added user examples

--- notify.py
+++ notify.py
@@ -1,33 +1,32 @@
 class Notify:
-    def __init__(self,name,order_made):
+    def __init__(self, name, order_made):
         self.name = name
         self.order_made = order_made
         self.is_read = False
 
-     
     def mark_as_read(self):
-       self.is_read = True
-   
+        self.is_read = True
+
     def send_notification(self):
-        item_order={
-            "name":self.name,
-            "orders":self.order_made
+        item_order = {
+            "name": self.name,
+            "orders": self.order_made
         }
         return item_order
-    
-    # method checks if there are new orders and if the notifications have been seen
-    
+
     def check_notifications(self):
         if self.order_made > 0 and not self.is_read:
-            print("Hello, you have {}  new orders from {}.".format(self.number_of_orders,self.name))
+            print("Hello, you have {} new orders from {}.".format(self.order_made, self.name))
         elif self.is_read:
-            print("You have no new notification")
-name = "Hellen"
-order_made = 5
+            print("You have no new notifications.")
 
-# instantiate a new variable and call the class
-new_message = Notification(name,order_made)
-notification_dict = new_message.send_notification()
-print(notification_dict)
+
+# Example usage:
+name = input("Enter your name: ")
+order_made = int(input("Enter the number of orders made: "))
+
+new_notification = Notify(name, order_made)
+
+new_notification.check_notifications()
 new_message.check_notifications()
 new_message.mark_as_read()
