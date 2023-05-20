9655ff6140167eab018e181db95611682b616c21
Masian-Parkire <124132282+Masian-Parkire@users.noreply.github.com>
2023-05-20 23:00:37 +0300

    wrote methods to enable a user make order, show payment message and their delivery addres. Also added instances.s

--- order.py
+++ order.py
@@ -1,33 +1,26 @@
 class Orders:
-    def __init__(self,name,order,payment,deliveryAdress) :
-        self.order=order
-        self.name=name
-        self.payment=payment
-        self.deliveryAddress=deliveryAdress
+    def __init__(self, name, order, payment, delivery_address):
+        self.order = order
+        self.name = name
+        self.payment = payment
+        self.delivery_address = delivery_address
         
-        # dictionary of the order attribute 
     def add_order(self):
-        orders_dict={
-           "name":self.name,
-           "order":self.order,
-           "payment":self.payment,
-           "deliveryAdress":self.deliveryAddress,
-          
+        orders_dict = {
+           "name": self.name,
+           "order": self.order,
+           "payment": self.payment,
+           "delivery_address": self.delivery_address,
        }
         return orders_dict
     
-    # instructs user to input their details
-name=input("Input your name")
-order=input("Input the type of order")
-payment=input("Input the type of payment")
-deliveryAddress=input("Input your delivery address")
+# Example usage:
+name = input("Input your name: ")
+order = input("Input the type of order: ")
+payment = input("Input the type of payment: ")
+delivery_address = input("Input your delivery address: ")
 
-    # User example
-new_Customer=Orders(Name,order,payment,deliveryAddress)
-user=new_Customer
-print(new_Customer.add_order())
-        order = Order(user, items)
-        self.orders.append(order)
-        return order
+new_order = Orders(name, order, payment, delivery_address)
+print(new_order.add_order())
 
 
