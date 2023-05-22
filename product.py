eb26a8717ee14241c762637b29d9f3f4f3a45f05
Purity-Wanjiku <124130591+Purity-Wanjiku@users.noreply.github.com>
2023-05-22 18:42:01 +0300

    wrote methods to enable a user add and remove products as well as search for the availability of a product.

--- product.py
+++ product.py
@@ -1,26 +1,28 @@
 class Product:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
         self.products = []
         
     def add_product(self, name):
+        self.name = name
         self.products.append(name)
         return f"{name} has been added to {self.name}."
     
     def search_product(self, name):
+        self.name = name
         for product in self.products:
             if product == name:
                 return f"{product} is available"
         return f"{product} is not available"
     
     def remove_product(self, product):
+        self.product = product
         if product in self.products:
             self.products.remove(product)
             return f"{product} has been removed from products"
         else:
             return f"{product} is not in products."
         
-my_object = Product("")
+my_object = Product()
 
 # Add some products to the Product 
 my_object.add_product("Orange")
@@ -38,5 +40,10 @@ print(my_object.remove_product("Pears"))
 
 # Check if the product has been removed
 print(my_object.products)
+
+
+
+
+
 
 
