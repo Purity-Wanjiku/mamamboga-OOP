3f241ac2d18a4854c535778d8646627bdae5bf35
Purity-Wanjiku <124130591+Purity-Wanjiku@users.noreply.github.com>
2023-05-22 08:05:00 +0300

    Wrote methods to enable my user add and remove items as well as search for the availability of a product.

--- product.py
+++ product.py
@@ -1,40 +1,27 @@
 class Product:
-    def __init__(self, name, amount):
-        self.name = name
-        self.amount = amount
-    
-    def check_availability(self):
-     if self.amount <= 0:
-        return f"Sorry, the {self.name} is not available."
-     elif self.amount > 0:
-        return f"The {self.name} is available."
-my_object =  Product("Orange",40)
-print(my_object.check_availability())
-
-my_object = Product("Tomoko",False)
-print (my_object.check_availability())
-my_object = Product("Guava",True)
-print (my_object.check_availability())
-class ProductManager:
     def __init__(self, name):
         self.name = name
         self.products = []
-    def add_product(self, name, available):
-        product = Product(name, available)
+        
+    def add_product(self, name):
+        product = Product(name)
         self.products.append(product)
         return "Item added successfully."
+    
     def search_product(self, name):
         for product in self.products:
-         if product.name == name:
-                return product.check_availability()
-        return f"Sorry, the {self.name} is not found."
-# Instantiate an object of ProductManager
-product_manager = ProductManager("Fruit")
-# Add some products to the product manager
-product_manager.products.append(Product("Mango", True))
-product_manager.products.append(Product("Pineapple", False))
-product_manager.products.append(Product("Watermelon", True))
+         if product == name:
+            return product
+        return None
+    
+my_object =  Product("")
+# Add some products to the Product 
+my_object.products.append("Orange")
+my_object.products.append("Pineapple")
+my_object.products.append("Watermelon")
 # Search for a product
-print(product_manager.search_product("Mango"))
-print(product_manager.search_product("Watermelon"))
+print(my_object.search_product("Mango"))
+print(my_object.search_product("Pineapple"))
+print(my_object.search_product("Watermelon"))
+print(my_object.products)
 
