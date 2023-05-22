11965bc801dbe520f73d9293147eba1b933ed166
Purity-Wanjiku <124130591+Purity-Wanjiku@users.noreply.github.com>
2023-05-22 18:02:47 +0300

    wrote methods to enable my user add, remove and check for the availability of a product in their stock.

--- product.py
+++ product.py
@@ -5,23 +5,39 @@ class Product:
         
     def add_product(self, name):
         product = Product(name)
-        self.products.append(product)
-        return "Item added successfully."
+        self.products.append(name)
+        return f"{name} has been added to {self.name}."
     
     def search_product(self, name):
         for product in self.products:
-         if product == name:
-            return product
-        return None
+            if product == name:
+                return f"{product} is available"
+        return f"{product} is not available"
     
-my_object =  Product("")
+    def remove_product(self, product):
+        if product in self.products:
+            self.products.remove(product)
+            return f"{product} has been removed from products"
+        else:
+            return f"{product} is not in products."
+        
+my_object = Product("")
+
 # Add some products to the Product 
-my_object.products.append("Orange")
-my_object.products.append("Pineapple")
-my_object.products.append("Watermelon")
+my_object.add_product("Orange")
+my_object.add_product("Pineapple")
+my_object.add_product("Watermelon")
+
 # Search for a product
 print(my_object.search_product("Mango"))
 print(my_object.search_product("Pineapple"))
 print(my_object.search_product("Watermelon"))
+
+# Remove a product
+print(my_object.remove_product("Orange"))
+print(my_object.remove_product("Pears"))
+
+# Check if the product has been removed
 print(my_object.products)
+
 
